b5a07a999602448878923fe051372da213f6b72f
Nicholas Ferrara <nferrara100@gmail.com>
2024-02-01 22:34:23 +0000

    feat: pico css

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,10 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"
+        />
         <Meta />
         <Links />
       </head>
