e512fb91b37e0290d94611dfb56dbedd246a811f
Nicholas Ferrara <nferrara100@gmail.com>
2024-02-20 10:24:55 +0000

    fix: always use dark color scheme

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
