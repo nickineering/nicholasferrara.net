0c85e4c1f4ad912817e157c10d1dc09c063e5379
Nicholas Ferrara <nferrara100@gmail.com>
2024-02-21 20:02:28 +0000

    fix: reworded bio

--- app/components/Bio.tsx
+++ app/components/Bio.tsx
@@ -5,19 +5,18 @@ export default function Bio() {
     <div className={styles.bio}>
       <p>Hey, I&apos;m Nick 👋</p>
       <p>
-        Ever since I was a teenager I&apos;ve been fascinated by code.
-        Practically everything in modern life relies on it, but less than 1% of
-        the world&apos;s workforce actually write it. The spectacular efficiency
-        of so few solving problems for so many is what drew me to software
-        engineering and keeps me going today.
+        Ever since I was a teenager I&apos;ve been fascinated by code. Less than
+        1% of the world&apos;s workforce actually write it, but collectively it
+        enables the modern way of life. That spectacular efficiency is what drew
+        me to software engineering and keeps me going today.
       </p>
       <p>
-        I like to avoid work for work&apos;s sake, and instead focus on
-        outcomes. The best way to do that is to find force multipliers, and
-        engineering has many of those. I enjoy mentoring and collaborating with
-        other engineers, pushing the limits of what is possible to automate, or
-        better yet staying close to the product so I can quickly realise when
-        code does not have to be written at all.
+        I like to avoid work for work&apos;s sake, and instead focus on the work
+        that makes a difference. The best way to do that is to find force
+        multipliers, and engineering has many of those. I enjoy mentoring and
+        collaborating with other engineers, pushing the limits of what is
+        possible to automate, or better yet staying close to the product so I
+        can quickly realise when code does not have to be written at all.
       </p>
       <p>
         It&apos;s great to meet like minded people, so if you&apos;ve made it
